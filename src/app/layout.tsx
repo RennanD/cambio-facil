c3a51df1970489f6ef3fa8f3fa21d437974934e3
RennanD <rplaykl@gmail.com>
2025-03-13 14:36:20 -0300

    fix: lint check

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,9 @@
-import { Geist, Geist_Mono } from 'next/font/google'
+import { Inter } from 'next/font/google'
 import './globals.css'
 import React from 'react'
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
+const fontSans = Inter({
+  variable: '--font-inter-sans',
   subsets: ['latin'],
 })
 
@@ -20,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${fontSans.variable} antialiased`}
       >
         {children}
       </body>
